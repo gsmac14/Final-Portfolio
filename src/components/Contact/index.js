58898c7017bc042606ee9ec75cc4c42ae7cde795
Girverius <gsmac1224@gmail.com>
2021-04-12 22:22:32 -0500

    fixed contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,28 +1,57 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import emailjs from 'emailjs-com';
 
-function Contact (){
 
-    return(
- <div className= "contact">
-  <div class="container">
-    <h1 class="display-4">Contact Me</h1>
-    <p class="lead" style={{textAlign: "center"}}>Allow one to two business days!</p>
-  </div>
+function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("")
 
-     <div className="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" acti></input>
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-</div>
- 
-    );
-}
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name, email, message);
+    //clears the input
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
 
+  function sendEmail(e) {
+    e.preventDefault(name, email, message);
+
+    emailjs.sendForm('service_cud2i6m', 'template_r4c6dwx', e.target, 'user_Zq2hhsqAB2qox3FkQuK6y')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail} >
+      <h1> Contact</h1>
+      <div>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" class="form-control form-control-lg" placeholder="Name" value={name} onChange={(e) => setName(e.target.value) } />
+
+      </div>
+      <div>
+
+      <label>Email</label>
+      <input type="email" name="user_email" class="form-control form-control-lg" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+
+      <label>Message</label>
+      <textarea name="message" class="form-control form-control-lg " placeholder="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
+      </div>
+      <button type="submit" onClick={handleSubmit} class="btn btn-success">Submit</button>
+    </form>
+  );
+  }
+
+  
 export default Contact;
 
 
